5006dfc2544f553185fcbb64a543826f39871ee8
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-04 20:19:17 +0530

    .

---
+++ Day-3/Day-3.js
@@ -0,0 +1,4 @@
+var app = angular.module("myApp",[]);
+app.controller("showDetailsController",function ($scope) {
+  $scope.persons=[{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28},{name:"Suraj",age:25},{name:"Sugam",age:24},{name:"mayank",age:28}];
+})
